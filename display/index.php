3f96a2ec8b11bbd967391d82dfe9db1dc9418883
flobo593 <git@floribo.de>
2021-04-23 23:15:08 +0200

    syntax complete

--- display/index.php
+++ display/index.php
@@ -2,6 +2,9 @@
 <html>
 <head>
   <script src="../data/functions.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    document.getElementById("output").innerHTML = line(isIn);
+  </script>
 </head>
 <body>
   <div id="output"></div>
