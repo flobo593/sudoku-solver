a4e9e3c19c8e4b635cbd459b9e77d062b32115dc
flobo593 <git@floribo.de>
2021-04-23 22:54:27 +0200

    line()-function

---
+++ data/functions.js
@@ -0,0 +1,26 @@
+function init() {
+
+}
+
+
+function 	preprocessing(sudoku) {
+}
+
+function line(input, lineNr) {
+  var length = input.length;
+  var storageLine = "";
+  for (var i = 1; i < input.length; i++) {
+    if (input[i] == "") {
+      continue;
+    }
+    else {
+      storageLine += i;
+    }
+  }
+  return storageLine;
+}
+
+in = ["", "", 2, 5, 1, 7, 3, "", 8];
+
+
+document.getElementById("output").innerHTML(line(in));
---
+++ display/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="../data/functions.js" type="text/javascript"></script>
+</head>
+<body>
+  <span id="output"></span>
+</body>
+</html>
