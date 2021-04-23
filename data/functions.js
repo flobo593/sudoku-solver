c403985e19e6d0c9759a1f9b99a9ab30d2d17755
flobo593 <git@floribo.de>
2021-04-23 23:09:11 +0200

    fixing sytax error 4.0

--- data/functions.js
+++ data/functions.js
@@ -20,7 +20,7 @@ function line(input, lineNr) {
   return storageLine;
 }
 
-in = ["", "", 2, 5, 1, 7, 3, "", 8];
+isIn = ["", "", 2, 5, 1, 7, 3, "", 8];
 
 
-document.getElementById("output").innerHTML(line(in));
+document.getElementById("output").innerHTML(line(isIn));
